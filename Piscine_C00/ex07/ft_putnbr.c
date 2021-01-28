c6fbfea42e00209a0b572459e6e7aef00cdf5506
Anne De mollerat <ade-moll@c2r12p1.42nice.fr>
2021-01-28 10:07:01 +0100

    RR

--- Piscine_C00/ex07/ft_putnbr.c
+++ Piscine_C00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ade-moll <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 11:04:57 by ade-moll          #+#    #+#             */
-/*   Updated: 2021/01/20 14:10:11 by ade-moll         ###   ########.fr       */
+/*   Updated: 2021/01/28 09:42:53 by ade-moll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	long int a;
+	unsigned int a;
 
-	a = nb;
-	if (a < 0)
+	a = 0;
+	if (nb < 0)
 	{
-		ft_putchar('_');
-		a = -a;
+		ft_putchar('-');
+		nb = -nb;
 	}
+	a = nb;
 	if (a > 9)
 	{
 		ft_putnbr(a / 10);
-		ft_putnbr(a % 10);
 	}
-	else
-		ft_putchar(a + '0');
+		a = a % 10 + '0';
+		ft_putchar(a);
 }
