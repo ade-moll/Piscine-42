2fc57b765a2620a3494ddfbac3222c3ab094c375
Anne De mollerat <ade-moll@c2r8p5.42nice.fr>
2021-02-01 13:24:50 +0100

    aa

--- Piscine_C08/ex00/ft.h
+++ Piscine_C08/ex00/ft.h
@@ -1,13 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-moll <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/28 08:49:39 by ade-moll          #+#    #+#             */
-/*   Updated: 2021/01/28 08:49:44 by ade-moll         ###   ########.fr       */
+/*   Created: 2021/02/01 11:49:12 by ade-moll          #+#    #+#             */
+/*   Updated: 2021/02/01 12:09:22 by ade-moll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
 
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
 
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
